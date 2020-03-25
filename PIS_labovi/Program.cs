ef89cc4cf5fd826526559aaf28018f54244de3d1
nikolapivac <npivac00@fesb.hr>
2020-03-25 14:36:33 +0100

    Add project files.

---
+++ PIS_labovi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS_labovi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
